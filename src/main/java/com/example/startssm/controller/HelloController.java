e8c0cc8c7959988dc3858c314dbaaf47c3f0f110
jiangdk <jiangdk@e-lead.cn>
2017-10-25 18:09:31 +0800

    上传了

--- src/main/java/com/example/startssm/controller/HelloController.java
+++ src/main/java/com/example/startssm/controller/HelloController.java
@@ -3,19 +3,43 @@ package com.example.startssm.controller;
 import com.example.startssm.GirlPropertice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-//    @Value("${cupSize}")
+    //    @Value("${cupSize}")
 //    private String cupSize;
     @Autowired
     private GirlPropertice girlPropertice;
 
     @RequestMapping(value = "/hello")
-    public String hello(){
-        return "Hello Spring Boot"+girlPropertice.getCupSize();
+    public String hello() {
+        return "Hello Spring Boot" + girlPropertice.getCupSize();
     }
+
+    @RequestMapping(value = "/pathvari/{id}")
+    public String loadPathVari(@PathVariable("id") Integer id) {
+        return "id" + id;
+    }
+
+    @RequestMapping(value = "/{id}/pathvaritwo")
+    public String loadPathVariTwo(@PathVariable("id") Integer id) {
+        return "id" + id;
+    }
+
+    @RequestMapping(value = "/requestparams")
+    public String loadRequestParam(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id) {
+        return "id" + id;
+    }
+    @GetMapping(value = "/get")
+    public String getLoad(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id) {
+        return "id" + id;
+    }
+    @PostMapping(value = "/post")
+    public String postLoad(@RequestParam(value = "id", required = false, defaultValue = "0") Integer id) {
+        return "id" + id;
+    }
+
+
 }
